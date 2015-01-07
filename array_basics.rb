69666d5ce737d827d3babc5213ca7dcc64f03b75
Andrew Overby <andrewdoverby@gmail.com>
2015-01-07 00:13:41 -0700

    part two: strings

--- array_basics.rb
+++ array_basics.rb
@@ -1,3 +1,4 @@
+#WIP in plain text
 class SimpleArray
   attr_reader :data
 
@@ -27,7 +28,29 @@ an_array("").uniq
     A) .size
     B) .length
 
-
   Part 2: Manipulating Strings--
 
-  1)
+1)  a_string.chars.size
+
+2)
+  A)    a_string.partition("o")   #almost there
+
+# B)    delete_at   last any digit regexp within nested block
+#       a_string = "hello"
+#       a_string.chars.delete_if do |x| x x=(/\d/) end
+
+  #C)  "hello".rindex(/[a..z]/, -1)
+
+  D)  a_string.chars.fetch(-1)
+
+      a_string.chars.fetch(-1) if a_string[-1]   #not quite
+
+3)
+  A) a_string = "hello"
+
+    a_string.replace "HELLO"
+
+  +
+      a_string + second_string
+
+      
